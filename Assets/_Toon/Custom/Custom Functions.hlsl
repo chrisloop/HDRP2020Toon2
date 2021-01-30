ea38d02148cbdd7edbffd1b5e1d6a9265dbc742a
chrisloop <chris@createaloop.org>
2021-01-29 19:25:23 -0600

    first commit

---
+++ Assets/_Toon/Custom/Custom Functions.hlsl
@@ -0,0 +1,124 @@
+#ifndef SHADERGRAPH_PREVIEW
+
+
+    #include "Packages/com.unity.render-pipelines.high-definition/Runtime/Material/NormalBuffer.hlsl"
+    #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+    #include "Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/LightDefinition.cs.hlsl"
+    #include "Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/Shadow/HDShadowManager.cs.hlsl"
+    #include "Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/Shadow/HDShadowSampling.hlsl"    
+    #include "Packages/com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/ShaderVariables.hlsl"
+    #include "Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoopDef.hlsl"
+
+void GetDepthNormal_float(float2 ScreenPosition, out float Depth, out float3 Normal)
+    {
+        Depth = SampleCameraDepth(ScreenPosition);
+        NormalData normalData;
+        DecodeFromNormalBuffer(_ScreenSize.xy * ScreenPosition, normalData);
+        Normal = normalData.normalWS;     
+    }
+#endif
+
+void  GetShadow_float(float2 uv, out float Shadow)
+{
+    Shadow = 1;
+
+    #ifndef SHADERGRAPH_PREVIEW
+        //Shadow = SAMPLE_TEXTURE2D_X_LOD(_CachedShadowmapAtlas, s_linear_clamp_sampler, uv, 0.0);
+    #endif 
+}
+
+void Sample_float(float2 uv, out float3 Sample)
+{
+    Sample = 1;
+
+    #ifndef SHADERGRAPH_PREVIEW
+       //Sample = SAMPL E_TEXTURE2D_X_LOD(_AmbientOcclusionTexture, s_linear_clamp_sampler, uv, 0.0);
+       Sample = SAMPLE_TEXTURE2D_X_LOD(_ScreenSpaceShadowsTexture, s_linear_clamp_sampler, uv, 0.0);
+       //PositionInputs posInput;
+       //posInput.positionSS = uv;
+       //Sample = GetScreenSpaceShadow(posInput, 1);
+    #endif
+}
+
+
+void GetSun_float(out float3 LightDirection, out float3 Color)
+{
+    LightDirection = float3(0.5, 0.5, 0);
+    Color = 1;
+
+    #ifndef SHADERGRAPH_PREVIEW
+        if (_DirectionalLightCount > 0)
+        {
+            DirectionalLightData light = _DirectionalLightDatas[0];
+            LightDirection = -light.forward.xyz;
+            Color = light.color;
+        }
+
+        //context.shadowContext = InitShadowContext();
+
+       // HDShadowContext sc;
+      //  sc.shadowDatas;
+        //sc.shadowDatas;// = _HDShadowDatas;
+        //sc.directionalShadowData = _HDDirectionalShadowData[0];
+ 
+
+
+    #endif
+}
+
+void Edges_float(float2 ScreenPosition, float EdgeRadius, float DepthMultiplier, float DepthBias, float NormalMultiplier, float NormalBias, 
+    out float Depth, out float3 Normal, out float Edges)
+{
+    Normal = 1;
+    Depth = 0;
+    Edges = 1;
+
+    #ifndef SHADERGRAPH_PREVIEW
+
+    #define MAX_SAMPLES 8
+
+    // Neighbour pixel positions
+    static float2 samplingPositions[MAX_SAMPLES] =
+    {
+        float2( 1,  1),
+        float2( 0,  1),
+        float2(-1,  1),
+        float2(-1,  0),
+        float2(-1, -1),
+        float2( 0, -1),
+        float2( 1, -1),
+        float2( 1, 0),
+    };
+
+    float depthDifference = 0;
+    float normalDifference = 0;
+
+    // center position
+    GetDepthNormal_float(ScreenPosition, Depth, Normal);
+
+    float depthSample;
+    float3 normalSample;
+
+    for (int i = 0; i < MAX_SAMPLES; i++)
+    {
+        GetDepthNormal_float(ScreenPosition + samplingPositions[i] * EdgeRadius * _ScreenSize.zw, depthSample, normalSample);
+        depthDifference = depthDifference + Depth - depthSample;
+        normalDifference = normalDifference + Normal - normalSample;
+    }
+
+    // depth sensitivity
+    depthDifference = depthDifference * DepthMultiplier;
+    depthDifference = saturate(depthDifference);
+    depthDifference = pow(depthDifference, DepthBias);
+    float EdgeDepth = depthDifference;    
+
+    // normal sensitivity
+    normalDifference = normalDifference * NormalMultiplier;
+    normalDifference = saturate(normalDifference);
+    normalDifference = pow(normalDifference, NormalBias);
+    float EdgeNormal = normalDifference;    
+
+    Edges = max(EdgeDepth, EdgeNormal); 
+
+    #endif
+}
---
+++ Assets/_Toon/Custom/MainLightData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+[ExecuteInEditMode]
+public class MainLightData : MonoBehaviour
+{
+	public Light mainLight;
+
+	void OnEnable()
+	{
+		mainLight = GetComponent<Light>();
+	}
+
+	
+	void Update ()
+	{
+		//Shader.SetGlobalVector("_LightDirection", -transform.forward);
+		//Shader.SetGlobalColor("_LightColor", mainLight.color);
+        Shader.SetGlobalFloat("_LightIntensity", mainLight.intensity); // fix later to get intensity without a script
+	}
+}
